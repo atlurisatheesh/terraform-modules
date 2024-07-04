f68f335f46d3b2dca6b1886228a57fb24fee414a
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-07-04 19:37:13 +0530

    Auto push

--- modules/backend.tf
+++ modules/backend.tf
@@ -2,7 +2,7 @@ provider "aws" {}
 
 terraform {
   backend "s3" {
-    bucket = "ddev-terraform-modules"
+    bucket = "dev-terraform-modules"
     key    = "tfmodules/terraform.tfstate"
     region = "us-east-1"
     
